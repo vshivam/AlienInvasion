f155dcfa98999e2fcae6c9f84cf7c59d80c006a9
shivam <shivam.bitsgoa@gmail.com>
2014-12-12 16:46:44 +0800

    Finalising DynamixAuth JS

--- dynamix/dynamixAuth.js
+++ dynamix/dynamixAuth.js
@@ -1,4 +1,27 @@
+/**
+ * Copyright (C) The Ambient Dynamix Project
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * @fileoverview
+ * Remote Web pairing authentication library for Ambient Dynamix.
+ * Depends on a modified version of the JavaScript QRCode library : <a href="http://davidshimjs.github.io/qrcodejs/" target="_blank">http://davidshimjs.github.io/qrcodejs/</a>
+ * @author shivam verma
+ */
+
 if (typeof DynamixAuth === "undefined") {
+    /**
+      * @namespace DynamixAuth
+      **/
     var DynamixAuth = {
         uuid: null,
         authListener: null,
